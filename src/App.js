87779947186937d872ff137212c48bf8d9fef739
Alex Parij <alexander.parij@autodesk.com>
2018-12-18 20:02:50 -0500

    testing injected scope data

--- src/App.js
+++ src/App.js
@@ -6,11 +6,11 @@ import data from './data.json';
 class MyWidget extends Component {
   render() {
     console.log('ALEX--->7', 'App.js', ' render app');
-    console.log('WIDGET window', window.current_user);
     return (
       <div className="App">
         <header className="App-header">
           <p>I'm a React App hosted somewhere on Github !</p>
+          <h1> {this.props.config.current_user.login}</h1>
           <button onClick={this.onClick}> I'm A Widget!</button>
         </header>
         <Board data={data} draggable />
@@ -19,18 +19,15 @@ class MyWidget extends Component {
   }
 
   onClick = () => {
-    console.log('ALEX--->29', 'App.js', 'Click');
-
     alert('helllo widget');
   };
 }
 
-const container = document.querySelector('#widget');
+//const container = document.querySelector('#widget');
 
 //ReactDOM.render(<App />, container);
 //export const myApp = ReactDOM.createPortal(<App />, container);
 export const init = config => {
-  console.log('ALEX--->40', 'App.js', 'init before render ', config);
   const container = document.querySelector(config.container);
   ReactDOM.render(<MyWidget config={config} />, container);
 };
